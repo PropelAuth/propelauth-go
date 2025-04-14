68f03209a63e0a683a1657988b897aa549957baf
Matthew Mauer <mrmauer112@gmail.com>
2025-04-14 13:03:08 -0400

    correctly parse OrgMetadata.Name from responses

--- pkg/models/org.go
+++ pkg/models/org.go
@@ -29,7 +29,7 @@ type OrgCompleteMetadata struct {
 // OrgMetadata has the information about the organziation.
 type OrgMetadata struct {
 	OrgID                 uuid.UUID              `json:"org_id"`
-	Name                  string                 `json:"org_name"`
+	Name                  string                 `json:"name"`
 	MaxUsers              *int                   `json:"max_users"`
 	Metadata              map[string]interface{} `json:"metadata"`
 	IsSamlConfigured      bool                   `json:"is_saml_configured"`
